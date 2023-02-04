75186922ba3527de094b972c17300ec301412ff1
Hasan Huda <hasanhuda037@gmail.com>
2023-02-03 20:01:19 -0500

    completed assignment first version

--- First.java
+++ First.java
@@ -2,6 +2,8 @@ package Practice.FirstJavaProgram;
 
 public class First{
   public static void main(String[] args){
-    
+    System.out.println("My name is Hasan Huda");
+    System.out.println("I am 21 years old");
+    System.out.println("MY hometown is Queens, NYC");
   }
 }
